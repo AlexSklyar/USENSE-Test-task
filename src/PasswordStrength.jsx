26a4c4a106b2c365c0e6ff4b416479d21602679f
Alex <80081422+AlexSklyar@users.noreply.github.com>
2023-02-26 13:30:26 +0200

    update return

--- src/PasswordStrength.jsx
+++ src/PasswordStrength.jsx
@@ -1,22 +1,28 @@
 import React, { useState } from 'react';
 
+
+const styles = {
+  less: {backgroundColor: "red"},
+  weak: {backgroundColor: "red"},
+  medium: {backgroundColor: "yellow"},
+  strong: {backgroundColor: "green"},
+}
+
 function PasswordStrength() {
   const [password, setPassword] = useState('');
-
   function handlePasswordChange(event) {
     setPassword(event.target.value);
   }
-
   function calculatePasswordStrength() {
     if (password.length === 0) {
       return 'none';
     } else if (password.length < 8) {
+      return 'less';
+    } else if (password.length >= 8 && /^[a-zA-Z]+$/.test(password) || /^\d+$/.test(password) || /^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+$/.test(password)) {
       return 'weak';
-    } else if (/^[a-zA-Z]+$/.test(password) || /^\d+$/.test(password) || /^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+$/.test(password)) {
-      return 'weak';
-    } else if (/^[a-zA-Z0-9]+$/.test(password) || /^[a-zA-Z!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+$/.test(password) || /^\d!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+$/.test(password)) {
+    } else if (password.length >= 8 && /^[a-zA-Z0-9]+$/.test(password) || /^[a-zA-Z!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+$/.test(password) || /^\d!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+$/.test(password)) {
       return 'medium';
-    } else {
+    } else{
       return 'strong';
     }
   }
@@ -25,9 +31,35 @@ function PasswordStrength() {
     <div className="password-strength">
       <label htmlFor="password">Password:</label>
       <input type="password" id="password" value={password} onChange={handlePasswordChange} />
-      <div className={`password-strength__block ${calculatePasswordStrength() === 'weak' ? 'password-strength__block--weak' : ''}`}></div>
-      <div className={`password-strength__block ${calculatePasswordStrength() === 'medium' ? 'password-strength__block--medium' : ''}`}></div>
-      <div className={`password-strength__block ${calculatePasswordStrength() === 'strong' ? 'password-strength__block--strong' : ''}`}></div>
+      <div className={'password-strength__block'} 
+
+      style={
+        calculatePasswordStrength() === 'weak'?
+        styles.weak : 
+        calculatePasswordStrength() === 'medium'? 
+        styles.medium  : 
+        calculatePasswordStrength() === 'strong'? 
+        styles.strong  : 
+        calculatePasswordStrength() === 'less'?
+        styles.less : styles.none
+        }></div>
+
+      <div className={'password-strength__block'} 
+      style={
+        calculatePasswordStrength() === 'medium'? 
+        styles.medium  : 
+        calculatePasswordStrength() === 'strong'? 
+        styles.strong  : 
+        calculatePasswordStrength() === 'less'?
+        styles.less : styles.none
+        }></div>
+
+      <div className={'password-strength__block'} style={
+        calculatePasswordStrength() === 'strong'? 
+        styles.strong  : 
+        calculatePasswordStrength() === 'less'?
+        styles.less : styles.none
+        }></div>
     </div>
   );
 }
